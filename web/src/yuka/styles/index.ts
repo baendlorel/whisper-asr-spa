9ed7eca347fb70b4df35af3c32f3226a0e9dc557
Aldia <futami16237@gmail.com>
2025-02-15 02:40:15 +0800

    Feat: 实现css自动引入，但可能还是需要手写

--- web/src/yuka/styles/index.ts
+++ web/src/yuka/styles/index.ts
@@ -1,8 +1,3 @@
-import button from './button.css?raw';
-import dialog from './dialog.css?raw';
-import label from './label.css?raw';
-import radio from './radio.css?raw';
-import select from './select.css?raw';
-import theme from './theme.css?raw';
-
-console.log('import.meta.glob', import.meta.glob('./*.css?raw'));
+Object.entries(import.meta.glob('./*.css')).forEach(([, value]) => {
+  value();
+});
