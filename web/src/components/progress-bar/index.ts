7cbb1adab098469c4e6b5969942a7fd4a58c75c7
aldia_mechdream <futami16237@gmail.com>
2025-11-04 22:10:55 +0800

    feat: 以ktjs编写了progressbar

--- web/src/components/progress-bar/index.ts
+++ web/src/components/progress-bar/index.ts
@@ -1,39 +1,25 @@
-import { useYuka, Yuka, I18NConfig } from '@/yuka';
-import style from './style.css?raw';
+import { div, span } from 'kt.js';
 
-const { h, css } = useYuka();
+export class ProgressBar {
+  readonly el: HTMLDivElement;
+  private readonly bar: HTMLDivElement;
+  private readonly label: HTMLSpanElement;
+  private readonly percent: HTMLSpanElement;
 
-css(style);
+  constructor() {
+    this.label = span({ style: { paddingRight: '5px' } });
+    this.percent = span();
+    this.bar = div('progress-bar', [this.label, this.percent]);
+    this.el = div({ class: 'progress-wrapper' }, this.bar);
+  }
 
-export default () => {
-  let progressBar: Yuka<HTMLDivElement>;
-  let spanLabel: Yuka<HTMLSpanElement>;
-  let spanPercent: Yuka<HTMLSpanElement>;
-
-  const component = h('div', { class: 'progress-wrapper' }).append(
-    (progressBar = h('div', 'progress-bar')).append(
-      (spanLabel = h('span', { style: { paddingRight: '5px' } })),
-      (spanPercent = h('span'))
-    )
-  );
-
-  /**
-   * 设置进度条百分比
-   * @param percent 0~1之间的小数
-   */
-  const setProgress = (percent: number) => {
+  set(percent: number) {
     const p = (percent * 100).toFixed(2) + '%';
-    progressBar.style.width = p;
-    spanPercent.text = p;
-  };
+    this.bar.style.width = p;
+    this.percent.textContent = p;
+  }
 
-  const setLabel = (i18n: I18NConfig) => {
-    spanLabel.i18n = i18n;
-  };
-
-  return {
-    component,
-    setProgress,
-    setLabel,
-  };
-};
+  setLabel(text: string) {
+    this.label.textContent = text;
+  }
+}
